e3ed22df83d1e69df9e8427a0f0b41ca259f2ab1
anderson029 <andersonfoliveira@yahoo.com>
2025-01-02 13:45:44 -0300

    add html report

--- src/test/java/stepDefinitions/RunCucumberTest.java
+++ src/test/java/stepDefinitions/RunCucumberTest.java
@@ -12,7 +12,7 @@ import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "stepDefinitions")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, json:target/cucumber-reports")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, json:target/cucumber-reports/cucumber.json, html:target/cucumber-reports/cucumber.html")
 
 public class RunCucumberTest {
 }
